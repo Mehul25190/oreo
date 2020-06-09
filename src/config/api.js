1133367a38e85c6e110137e21ee5e413743384ba
Mehul Patel <mehul@Mehuls-MacBook-Air.local>
2020-06-09 23:36:48 +0530

    first commit

---
+++ src/config/api.js
@@ -0,0 +1,10 @@
+export const API = 'http://universalwebtech.com/mehul_hosting/oreo';
+
+export const CONSUMER_KEY = 'ck_a638a536aafcd565f792b630e1a380a277ace74a';
+export const CONSUMER_SECRET = 'cs_741131a04d797b151970e2ceb84fd323f8a93653';
+
+export default {
+  API_ENDPOINT: API,
+  CONSUMER_KEY,
+  CONSUMER_SECRET
+};
