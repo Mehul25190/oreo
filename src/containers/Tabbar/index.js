e7359425fe9d543eaf46f73cbabd07c0406d4b95
Mehul25190 <mehul25190@gmail.com>
2020-06-18 23:22:05 +0530

    Floating Button Code Continue

--- src/containers/Tabbar/index.js
+++ src/containers/Tabbar/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import {connect} from 'react-redux';
-import {TouchableOpacity, StyleSheet} from 'react-native';
+import {TouchableOpacity, StyleSheet, View} from 'react-native';
 import {Text, SafeAreaView} from 'src/components';
 import IconTabbar from './IconTabbar';
 
@@ -13,11 +13,14 @@ import {grey5} from 'src/components/config/colors';
 import {sizes} from 'src/components/config/fonts';
 import {padding} from 'src/components/config/spacing';
 
-const Tabbar = (props) => {
+import ActionButton from 'react-native-action-button';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const Tabbar = props => {
   const {
     configs,
     screenProps: {t, theme},
-    navigation
+    navigation,
   } = props;
   const data = [
     {
@@ -58,35 +61,82 @@ const Tabbar = (props) => {
   ];
 
   return (
-    <SafeAreaView forceInset={{bottom: 'always'}} style={[styles.container, theme.TabNavigator.tabStyle]}>
-      {data.map((tab, index) => tab.isShow ? (
-        <TouchableOpacity
-          key={index}
-          style={styles.item}
-          onPress={() => navigation.navigate(tab.router)}
-        >
-          <IconTabbar
-            name={tab.iconName}
-            color={navigation.state.index === index ? theme.colors.primary:grey5}
-            nameData={tab.nameData}
-            {...tab.iconProps}
-          />
-          <Text medium style={[
-            styles.text,
-            {
-              color: navigation.state.index === index ? theme.colors.primary:grey5
-            },
-          ]}>{tab.name}</Text>
-        </TouchableOpacity>
-      ) : null)}
-    </SafeAreaView>
-  )
+    <View>
+      <View
+        style={{
+          justifyContent: 'flex-start',
+          alignContent: 'flex-start',
+         padding:31,
+         backgroundColor: 'transparent'
+       
+        }}>
+        <ActionButton buttonColor="rgba(231,76,60,1)">
+          {/*Inner options of the action button*/}
+          {/*Icons here https://infinitered.github.io/ionicons-version-3-search/*/}
+          <ActionButton.Item
+            buttonColor="#9b59b6"
+            title="Add to Watch Later"
+            onPress={() => alert('Added to watch later')}>
+            <Icon name="md-eye" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+          <ActionButton.Item
+            buttonColor="#3498db"
+            title="Add to Favourite"
+            onPress={() => alert('Added to favourite')}>
+            <Icon name="md-star" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+          <ActionButton.Item
+            buttonColor="#1abc9c"
+            title="Share"
+            onPress={() => alert('Share Post')}>
+            <Icon name="md-share-alt" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+        </ActionButton>
+      </View>
+      <SafeAreaView
+        forceInset={{bottom: 'always'}}
+        style={[styles.container, theme.TabNavigator.tabStyle]}>
+        {data.map((tab, index) =>
+          tab.isShow ? (
+            <TouchableOpacity
+              key={index}
+              style={styles.item}
+              onPress={() => navigation.navigate(tab.router)}>
+              <IconTabbar
+                name={tab.iconName}
+                color={
+                  navigation.state.index === index
+                    ? theme.colors.primary
+                    : grey5
+                }
+                nameData={tab.nameData}
+                {...tab.iconProps}
+              />
+              <Text
+                medium
+                style={[
+                  styles.text,
+                  {
+                    color:
+                      navigation.state.index === index
+                        ? theme.colors.primary
+                        : grey5,
+                  },
+                ]}>
+                {tab.name}
+              </Text>
+            </TouchableOpacity>
+          ) : null,
+        )}
+      </SafeAreaView>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
-    borderTopWidth: 1
+    borderTopWidth: 1,
   },
   item: {
     flex: 1,
@@ -97,13 +147,13 @@ const styles = StyleSheet.create({
     fontSize: sizes.h6 - 2,
     lineHeight: 15,
     marginTop: 5,
-  }
+  },
 });
 
 const mapStateToProps = state => {
   return {
-    configs: configsSelector(state)
-  }
+    configs: configsSelector(state),
+  };
 };
 
 export default connect(mapStateToProps)(Tabbar);
